e3d74846770eb95ca9e7c2e887f11bd68f4485b7
PdskDev <prestadesk@gmail.com>
2023-03-20 21:59:34 +0100

    Submit request

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -34,6 +34,20 @@ export class SignUpComponent implements OnInit {
     //this.buttonIsDisabled = this.password !== this.confirmPassword;
   }
 
+  onClickSignUp() {
+    fetch('/api/1.0/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
   isButtonDisabled() {
     if (
       this.password == this.confirmPassword &&
